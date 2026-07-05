aa63c53bf54c24aede123c7cfd8c79ac6c848038
JijeshMon <jijeshjmj@gmail.com>
2026-07-05 23:34:30 +0530

    docker added

--- zenvofurniture/src/main/java/com/zenvofurniture/config/CorsConfig.java
+++ zenvofurniture/src/main/java/com/zenvofurniture/config/CorsConfig.java
@@ -12,14 +12,8 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
             .allowedOrigins(
               
-            		"http://localhost:3000",      // Add your React port
-            		"http://localhost:3001",      // Add your React port
-            		"http://localhost:3002",      // Add your React port
-            		"http://localhost:3003",      // Add your React port
-                "http://192.168.1.80:3000",
-                "http://192.168.1.80:3001",
-                "http://192.168.1.80:3002",    // Add your network URL
-                "http://192.168.1.80:3003"    // Add your network URL
+            		"http://localhost:3000"      
+            		
             )
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
             .allowedHeaders("*")
